dcc94001295629bf7c1234452ef065bc01acf014
Rodrigo Teixeira <rdrgotxra@gmail.com>
2024-04-28 01:03:40 -0300

    completei insertion sort

--- estrutura_dados/bubblesort.py
+++ estrutura_dados/bubblesort.py
@@ -1,7 +1,6 @@
 nums = [74, 91, 47, 40, 67, 90, 71, 33, 83, 35]
 
-
-print(*nums, '\n')
+print(*nums, '<-desordenado\n')
 
 for i in range(len(nums)):
     for j in range(1, len(nums)-i):
@@ -9,5 +8,7 @@ for i in range(len(nums)):
             nums[j], nums[j-1] = nums[j-1], nums[j]
 
         print(*nums)
+    
+    print('')
 
-    print("\n")
+print('/\ ordenado')
--- estrutura_dados/insertionsort.py
+++ estrutura_dados/insertionsort.py
@@ -1,9 +1,25 @@
-# nums = 'RODRIGOCOST'
 nums = [74, 91, 47, 40, 67, 90, 71, 33, 83, 35]
+# nums = list('RODRIGOCOST')
+# nums = [9, 6, 5, 4, 2, 1]
 
+print(*nums, '<- desordenado\n')
 
-print(*nums, '\n')
-
+# i -> do 1 ao último
 for i in range(1, len(nums)):
     key = nums[i]
-    
+    print(f"\n({key}) <- chave\n") # debug
+
+    for j in range(i-1, -1, -1):
+        if key<nums[j] :
+            nums[j+1] = nums[j]
+            if j == 0:
+                nums[j] = key
+
+        else:
+            nums[j+1] = key
+            break
+
+        print(*nums, f'({nums[j]})')
+
+print('')
+print(*nums, '<- ordenado')
